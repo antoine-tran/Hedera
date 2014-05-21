e855089a499bbf8fab625d8f1cfd597a8de1cd6b
antoine-tran <tranatuan24@gmail.com>
2014-05-21 18:30:12 +0200

    refactor packages, add test mockups

--- java/main/org/hedera/pig/load/wikipedia/WikipediaRevisionPairLoaderTest.java
+++ java/main/org/hedera/pig/load/wikipedia/WikipediaRevisionPairLoaderTest.java
@@ -72,7 +72,7 @@ public class WikipediaRevisionPairLoaderTest extends LoadFunc implements LoadMet
 			if (hasNext) {
 				Text content = reader.getCurrentValue();
 				Document doc = Jsoup.parse(content.toString(), "", Parser.xmlParser());
-				Elements elems = doc.select("page > revision");	
+				/*Elements elems = doc.select("page > revision");	
 				DateTime dt = null;
 				for (Element e : elems) {
 					Elements subElems = e.getElementsByTag("timestamp");
@@ -88,7 +88,7 @@ public class WikipediaRevisionPairLoaderTest extends LoadFunc implements LoadMet
 						}
 					}
 					
-				}
+				}*/
 				return tuples.newTupleNoCopy(Arrays.asList(content.toString()));	
 			}
 		} catch (InterruptedException e) {
