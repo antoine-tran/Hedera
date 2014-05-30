9ee9e17b17b5ca848a79c13f427242988f9ea15b
antoine-tran <tranatuan24@gmail.com>
2014-05-30 17:52:52 +0200

    add temporal anchors extraction

---
+++ java/main/org/hedera/mapreduce/ExtractTemporalAnchorText.java
@@ -0,0 +1,20 @@
+package org.hedera.mapreduce;
+
+import org.apache.hadoop.util.Tool;
+
+import tuan.hadoop.conf.JobConfig;
+
+/**
+ * This jobs extract anchor text from Wikipedia revisions 
+ * @author tuan
+ *
+ */
+public class ExtractTemporalAnchorText extends JobConfig implements Tool {
+
+	@Override
+	public int run(String[] arg0) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
