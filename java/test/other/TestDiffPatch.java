54c2aed8b9fa0a96feefe5ae330adfe2f5001082
antoine-tran <tranatuan24@gmail.com>
2014-06-03 17:30:08 +0200

    update testcases

--- java/test/other/TestDiffPatch.java
+++ java/test/other/TestDiffPatch.java
@@ -17,17 +17,17 @@ public class TestDiffPatch {
 	@Test
 	public void testNewAlgo() {
 		List<String> original = new ArrayList<>();
-		//original.add("The quick brown fox jumps over the lazy dog");
-		//original.add("This sentence stays the same");
-		//original.add("But this will be removed.");
-		//original.add("This will be the same.");
+		original.add("The quick brown fox jumps over the lazy dog");
+		original.add("This sentence stays the same");
+		original.add("But this will be removed.");
+		original.add("This will be the same.");
 		
         List<String> revised  = new ArrayList<>();
-        //revised.add("And this has been added.");
-        //revised.add("That quick brown fox jumped over a lazy dog.");
-        //revised.add("This sentence stays the same");
-        //revised.add("And this has been newly added.");
-        //revised.add("This will be the same.");
+        revised.add("And this has been added.");
+        revised.add("That quick brown fox jumped over a lazy dog.");
+        revised.add("This sentence stays the same");
+        revised.add("And this has been newly added.");
+        revised.add("This will be the same.");
         
         // Compute diff. Get the Patch object. Patch is the container for computed deltas.
         difflib.Patch patch = DiffUtils.diff(original, revised);
