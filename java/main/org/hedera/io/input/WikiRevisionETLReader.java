74a1ebdd6a359b642b31bf73bfb6e1bd7deb654e
antoine-tran <tranatuan24@gmail.com>
2014-06-04 09:00:11 +0200

    add interface methods to ETLReader

--- java/main/org/hedera/io/input/WikiRevisionETLReader.java
+++ java/main/org/hedera/io/input/WikiRevisionETLReader.java
@@ -1,8 +1,49 @@
 package org.hedera.io.input;
 
+import java.io.IOException;
+
+import org.apache.hadoop.mapreduce.InputSplit;
 import org.apache.hadoop.mapreduce.RecordReader;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
 
 
 public class WikiRevisionETLReader<KEYIN, VALUEIN> extends RecordReader<KEYIN, VALUEIN> {
+
+	@Override
+	public void close() throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public KEYIN getCurrentKey() throws IOException, InterruptedException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public VALUEIN getCurrentValue() throws IOException, InterruptedException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public float getProgress() throws IOException, InterruptedException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void initialize(InputSplit arg0, TaskAttemptContext arg1)
+			throws IOException, InterruptedException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean nextKeyValue() throws IOException, InterruptedException {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
