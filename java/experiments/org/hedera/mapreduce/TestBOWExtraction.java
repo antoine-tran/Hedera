d5022b9f9a3ca6c169ac70c12159b9b8ccc08bca
antoine-tran <tranatuan24@gmail.com>
2014-06-09 11:46:14 +0200

    add test routines for BOW extraction

---
+++ java/experiments/org/hedera/mapreduce/TestBOWExtraction.java
@@ -0,0 +1,16 @@
+package org.hedera.mapreduce;
+
+import org.apache.hadoop.util.Tool;
+
+import tuan.hadoop.conf.JobConfig;
+
+/** Extracting the Bag-of-words for */
+public class TestBOWExtraction extends JobConfig implements Tool {
+
+	@Override
+	public int run(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
