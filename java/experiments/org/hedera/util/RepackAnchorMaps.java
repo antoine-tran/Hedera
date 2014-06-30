00f20f50a0b93f5073f15e156cddf817673f9470
antoine-tran <tranatuan24@gmail.com>
2014-06-30 13:13:18 +0200

    add util package

---
+++ java/experiments/org/hedera/util/RepackAnchorMaps.java
@@ -0,0 +1,10 @@
+package org.hedera.util;
+
+/** 
+ * This tool reads the mapping of Wikipedia entity ID - anchor mapping
+ * output from Cloud9, checks the continuous entity IDs and IDs of
+ * anchor texts, and repacks everything into continuous id ranges
+ */
+public class RepackAnchorMaps {
+
+}
