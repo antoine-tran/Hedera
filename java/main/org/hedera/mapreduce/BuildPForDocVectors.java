2c5388d976ddf34aac5e0af6cb43d907aabc9067
antoine-tran <tranatuan24@gmail.com>
2014-07-01 23:51:17 +0200

    debug vectorization

--- java/main/org/hedera/mapreduce/BuildPForDocVectors.java
+++ java/main/org/hedera/mapreduce/BuildPForDocVectors.java
@@ -40,6 +40,7 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 import org.apache.log4j.Logger;
@@ -273,7 +274,7 @@ public class BuildPForDocVectors extends JobConfig implements Tool {
 		job.getConfiguration().set(PREPROCESSING, preprocessing);
 
 		job.setInputFormatClass(WikiFullRevisionJsonInputFormat.class);
-		job.setOutputFormatClass(SequenceFileOutputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
 
 		job.setMapOutputKeyClass(LongWritable.class);
 		job.setMapOutputValueClass(IntArrayWritable.class);
