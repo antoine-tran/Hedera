74dcd43fbdbc779f287183de5c3b17cacf221c8b
antoine-tran <tranatuan24@gmail.com>
2014-07-04 14:25:05 +0200

    add boto logging

--- python/id2title.py
+++ python/id2title.py
@@ -5,10 +5,12 @@ Extract the mapping of wikipedia page id - title from the revision dumps
 from mrjob.job import MRJob
 import re
 
+import logging
+logging.basicConfig(filename='boto.log',level=logging.DEBUG)
+
+
 # set up classpath
 import os
-os.environ['HADOOP_HOME']='/opt/cloudera/parcels/CDH'
-os.environ['HADOOP_MAPRED_HOME']='/opt/cloudera/parcels/CDH-4.6.0-1.cdh4.6.0.p0.26/lib/hadoop-0.20-mapreduce'
 
 WORD_RE = re.compile(r"[\w']+")
 
@@ -16,11 +18,12 @@ import json
 
 class MRTitle2Id(MRJob):
 
-    def mapper(self, _, line):
+    def mapper(self, pid, line):
         obj = json.loads(line)
         pid = int(obj['page_id'])
         title = obj['page_title']
-        yield (pid, title)
+        # yield (pid, line)
+        yield (pid,title)
 
     def combiner(self, pid, title):
         d = set()
