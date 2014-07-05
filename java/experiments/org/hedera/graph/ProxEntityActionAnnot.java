d1041ece0d6b97b56593288757107b240078918d
antoine-tran <tranatuan24@gmail.com>
2014-07-05 07:47:30 +0200

    add code for contextualize the entity anchor

---
+++ java/experiments/org/hedera/graph/ProxEntityActionAnnot.java
@@ -0,0 +1,29 @@
+package org.hedera.graph;
+
+import org.apache.hadoop.util.Tool;
+
+import tuan.hadoop.conf.JobConfig;
+
+/** This tool builds the annotation of entities for each
+ * document vector based on the proximity heuristics
+ * 
+ * - Input: anchor / entity mapping, seed words, proximity 
+ * distance, entity repacked id list, repacked doc id list,
+ * dictionary
+ * 
+ * - Output: csv file
+ * [entity ID] TAB [word ID] TAB [timestamp]  
+ * - For every doc vector, if the anchor is found,
+ * then all target entities are annotated for the doc 
+ * 
+ * - If the seed word is specified and the proximity window
+ * is known, the */
+public class ProxEntityActionAnnot extends JobConfig implements Tool {
+
+	@Override
+	public int run(String[] arg0) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+}
