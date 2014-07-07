1d8a9edfcbf5cca62cb48356701feeb8d41affcc
antoine-tran <tranatuan24@gmail.com>
2014-07-07 16:08:05 +0200

    add a patch for the deprecated json2anchor

---
+++ python/patch_json2anchor.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+This is a patch to the buggy json2anchor python script (now fixed) that forgets
+to put a tab between the timestamp and the source ID
+
+Output anchor in format (separated by TAB)
+[timestamp] [source ID] [revision ID] [previous revision ID] [anchor text] [target title]
+"""
+
+from mrjob.job import MRJob
+from mrjob.protocol import RawProtocol
+from mrjob.protocol import RawValueProtocol
+
+# Enable boto logging for EMR debug
+import logging
+logging.basicConfig(filename='boto.log',level=logging.INFO)
+
+# Settings for running at the cluster
+import os
+os.environ['HADOOP_HOME']='/opt/cloudera/parcels/CDH'
+os.environ['HADOOP_MAPRED_HOME']='/opt/cloudera/parcels/CDH-4.6.0-1.cdh4.6.0.p0.26/lib/hadoop-0.20-mapreduce'
+
+
+class MRAnchorFix(MRJob):
+
+    INPUT_PROTOCOL = RawProtocol
+    INTERNAL_PROTOCOL = RawProtocol
+    OUTPUT_PROTOCOL = RawValueProtocol
+    
+    def mapper(self,pid,line):
+
+        # Patch fix: the first 13 characters in pid is timestamp
+        ts = pid[:12]
+        pageid = ts[12:]
+        line = pageid + '\t' + line        
+        yield (ts,line)
+
+    def reducer(self,tsid,lines):
+        for line in lines:
+            line = tsid + '\t' + line
+            yield (None,line)
+
+if __name__ == "__main__":
+    MRAnchorFix.run()
