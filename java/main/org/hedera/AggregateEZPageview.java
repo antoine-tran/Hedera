f0d4d1c768b948169eeca635d2b7b2468002edd7
antoine-tran <tranatuan24@gmail.com>
2014-12-05 00:49:29 +0100

    sync

--- java/main/org/hedera/AggregateEZPageview.java
+++ java/main/org/hedera/AggregateEZPageview.java
@@ -1,5 +1,16 @@
 package org.hedera;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import tuan.io.FileUtility;
+
 /**
  * Aggregate the page views after normalizing titles using LocalEZPageview program
  *  
@@ -8,10 +19,64 @@ package org.hedera;
  */
 public class AggregateEZPageview {
 
-	/** Input files are supposed to be sorted */
-	public static void main(String[] args) {
+	private static final DateTimeFormatter dtfMonth = DateTimeFormat
+			.forPattern("YYYY-mm");
+	private static final DateTimeFormatter dtfMonthPrinter = DateTimeFormat
+			.forPattern("YYYYmm");
+	/** Input files are supposed to be sorted 
+	 * @throws IOException */
+	public static void main(String[] args) throws IOException {
+		
+		DateTime month = dtfMonth.parseDateTime(args[2]);
+		int dayOfMonth = month.dayOfMonth(	).getMaximumValue();
+
+		int[] value = new int[dayOfMonth + 1];
+
+		int monthAsInt = Integer.parseInt(dtfMonthPrinter.print(month));
 		
 		String prevTitle = null;
-		
+		FileWriter writer = new FileWriter(args[1]);
+		Writer o = new BufferedWriter(writer);
+		for (String line : FileUtility.readLines(args[0])) {
+			int i = line.indexOf('\t');
+			i = line.indexOf('\t',i+1);
+			String titleAndMonth = line.substring(0,i);
+			if (prevTitle != null && !prevTitle.equals(titleAndMonth)) {
+				StringBuilder sb = new StringBuilder();
+				sb.append(prevTitle);
+				sb.append('\t');
+				sb.append(monthAsInt);
+				for (int item : value) {
+					sb.append('\t');
+					sb.append(item);
+				}
+				sb.append('\n');
+				o.write(sb.toString());
+				for (int j = 0; j < value.length; j++) {
+					value[j] = 0;
+				}
+			}
+			for (int j = 0, k = i, tmp = k; j < value.length; j++, k = tmp) {
+				tmp = line.indexOf('\t',k+1);
+				value[j] += Integer.parseInt(line.substring(k+1,tmp));
+			}
+			prevTitle = titleAndMonth;
+		}
+		if (prevTitle != null) {
+			StringBuilder sb = new StringBuilder();
+			sb.append(prevTitle);
+			sb.append('\t');
+			sb.append(monthAsInt);
+			for (int item : value) {
+				sb.append('\t');
+				sb.append(item);
+			}
+			sb.append('\n');
+			o.write(sb.toString());
+			for (int j = 0; j < value.length; j++) {
+				value[j] = 0;
+			}
+		}
+		o.close();
 	}
 }
