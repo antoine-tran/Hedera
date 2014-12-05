8f576266a99f3660bc54cdec911d5244cd92caf7
antoine-tran <tranatuan24@gmail.com>
2014-12-05 16:17:58 +0100

    add the implementation of stats.grok.se in my own way

---
+++ etc/match_wikistats.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# A work flow of normalizing, sorting, aggregating, matching wikistats
+
+# Input 1 is the wikistat file (pagecount-ez)
+# Input 2 is the month of wiki stat in "YYYY-mm" format
+# input 3 is the id-title map file [ format: title TAB id ]
+
+# Example sh match_wikistats.sh pagecounts-2013-12-ge.bz2 2013-12 title2id.20140502
+
+# Normalize the EZ page view
+sh run-local-jars.sh org.hedera.LocalEZPageview $1 $1.out $2
+
+# Sort by title
+LANG=en_EN sort -k1,1 $1.out > $1.sort
+
+# Aggregate the view counts
+sh run-local-jars.sh org.hedera.AggregateEZPageview $1.sort $1.aggr $2
+
+# Match against the title-id mapping
+LANG=en_EN join $3 $1.aggr | awk '{for(i=2;i<=NF;i++)printf "%s", $i (i==NF?ORS:OFS)}' > $1.ts
+
+# Remove the temporary file
+rm -rf $1.out
+rm -rf $1.sort
+rm -rf $1.aggr
