d58c28c1b9792f6620bfecc6cff9ea79fd48ebfb
Tuan <ttran@l3s.de>
2015-06-20 16:22:55 +0200

    fix redirect issues

--- etc/match_wikistats.sh
+++ etc/match_wikistats.sh
@@ -9,19 +9,25 @@
 # Example sh match_wikistats.sh pagecounts-2013-12-ge.bz2 2013-12 title2id.20140502
 
 # Normalize the EZ page view
-# sh $(pwd)/etc/run-local-jars.sh org.hedera.LocalEZPageviewDay $1 $1.out $2
-sh $(pwd)/etc/run-local-jars.sh org.hedera.LocalEZPageviewHour $1 $1.out $2
+sh $(pwd)/etc/run-local-jars.sh org.hedera.LocalEZPageviewDay $1 $1.out $2
+# sh $(pwd)/etc/run-local-jars.sh org.hedera.LocalEZPageviewHour $1 $1.out $2
 
 # Sort by title
 LANG=en_EN sort -k1,1 $1.out > $1.sort
 
 # Aggregate the view counts
-# sh $(pwd)/etc/run-local-jars.sh org.hedera.AggregateEZPageview $1.sort $1.aggr $2
+sh $(pwd)/etc/run-local-jars.sh org.hedera.AggregateEZPageview $1.sort $1.aggr $2
 
 # Match against the title-id mapping
-# LANG=en_EN join $3 $1.aggr | awk '{for(i=2;i<=NF;i++)printf "%s", $i (i==NF?ORS:OFS)}' > $1.ts
+LANG=en_EN join $3 $1.aggr | awk '{for(i=2;i<=NF;i++)printf "%s", $i (i==NF?ORS:OFS)}' > $1.ts
+
+# Another round of aggregating to resolve the redirects
+LANG=en_EN sort -k1,1 $1.ts > $1.sort
+
+# Aggregate the view counts
+sh $(pwd)/etc/run-local-jars.sh org.hedera.AggregateEZPageview $1.sort $1.ts $2
 
 # Remove the temporary file
-# rm -rf $1.out
-# rm -rf $1.sort
-# rm -rf $1.aggr
+rm -rf $1.out
+rm -rf $1.sort
+rm -rf $1.aggr
