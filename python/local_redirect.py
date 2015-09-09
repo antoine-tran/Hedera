abe28eef27f41c0a4c95d449c9011aea63cf972b
Tuan <ttran@l3s.de>
2015-09-09 17:32:09 +0200

    update local redirect

--- python/local_redirect.py
+++ python/local_redirect.py
@@ -9,37 +9,31 @@ import sys,codecs
 from collections import defaultdict
 
 # Input 1: Dictionary, input 2: redirect
-def redirect_chain(simpleredirectfile,outfile):
+def redirect_chain(simpleredirectfile,dictfile,outfile):
     t2id = defaultdict(int)
-    with codecs.open(simpleredirectfile,'r','utf-8') as reader:
+    with codecs.open(dictfile,'r','utf-8') as reader:
         for line in reader:
             i = line.rfind('\t')
-            cs = int(line.rstrip()[i+1:])
-            if cs == -1:
-                j = line.find('\t')
-                t2id[line[j+1:i]] = int(line[:j])
+            t2id[line[:i]] = int(line.rstrip()[i+1:])
+    redir = defaultdict(int)
     with codecs.open(simpleredirectfile,'r','utf-8') as reader:
-        o = codecs.open(outfile,'w','utf-8')
         for line in reader:
             i = line.rfind('\t')
             cs = int(line.rstrip()[i+1:])
             if cs == 1:
                 j = line.find('\t')
                 if line[j+1:i] in t2id:
-                    o.write('%d\t%d\n' % (int(line[:j]),t2id[line[j+1:i]]))
-        o.close()
+                    redir[int(line[:j])] = t2id[line[j+1:i]]
 
-# Change dictionary using the redirect mapping
-def resolve_redirect(dictfile,redirfile,outfile):
-    redir = defaultdict(int)
-    with codecs.open(redirfile,'r','utf-8') as reader:
-        for line in reader:
-            i = line.rfind('\t')
-            redir[int(line[:i])] = int(line.rstrip()[i+1:])
-    with codecs.open(dictfile,'r','utf-8') as reader:
-        for line in reader:
-            
-            
+    
+    with codecs.open(outfile,'w','utf-8') as o:
+        for t in t2id:
+            pid = t2id[t]
+            if not pid in redir:
+                o.write('%s %d\n' % (t.replace(' ','_'),pid))
+            else:
+                o.write('%s\t%d\n' % (t.replace(' ','_'),redir[pid]))
+
         
 if __name__ == '__main__':
-    redirect_chain(sys.argv[1],sys.argv[2])
+    redirect_chain(sys.argv[1],sys.argv[2],sys.argv[3])
