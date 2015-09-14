854f866e39610cb9021527955538eff0985a169f
Tuan <ttran@l3s.de>
2015-09-14 16:44:47 +0200

    change match_wikistat.sh to Python version

---
+++ python/AggregateEZPageview.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Python version of the Java class AggregateEZPageview
+Sep 14, 2015
+"""
+
+from collections import defaultdict
+import sys,os,codecs
+
+# m is the month size
+def aggr(infile,outfile,m):
+
+    # We load everything into the memory
+    ts = defaultdict(list)
+    with codecs.open(infile,'r','utf-8') as reader:
+        for line in reader:
+            i = line.find(' ')
+            ent = line[:i]
+            if not ent in ts:
+                ts[e] = [0]*(m+1)
+            cnt = 0
+
+            # skip month
+            i = line.find(' ',i+1)
+
+            while cnt < m+1:
+                j = line.find(' ',i+1)
+                ts[e][cnt] += int(line[i+1:j])
+                cnt += 1
+                i = j
+
+    with codecs.open(outfile,'w','utf-8') as o:
+        for k,v in ts.iteritems():
+            o.write(k)
+            [o.write(' d' % i) for i in v]
+            o.write('\n')
+
+if __name__ == "__main__":
+    aggr(sys.argv[1],sys.argv[2],int(sys.argv[3]))
---
+++ python/JoinEZPageview.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Join dictionary and the time series
+
+import sys,codecs
+from collections import defaultdict
+
+def join_ts_dict(tsfile,dictfile,outfile):
+    
+    # Hash-based join
+    t2id = defaultdict(int)
+    with codecs.open(dictfile,'r','utf-8') as reader:
+        for line in reader:
+            i = line.find(' ')
+            t2id[line[:i]] = int(line.rstrip()[i+1:])
+
+    with codecs.open(tsfile,'r','utf-8') as reader:
+        o = codecs.open(outfile,'w','utf-8')
+        for line in reader:
+            i = line.find(' ')
+            ent = line[:i]
+            if ent in t2id:
+                o.write(t2id[ent])
+                o.write(line[i+1:])
+        o.close()
+
+if __name__ == "__main__":
+    join_ts_dict(sys.argv[1],sys.argv[2],sys.argv[3])
