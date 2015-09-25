2a97a0c6d435a0cda8c01cbaf5520e937ed6140f
Tuan <ttran@l3s.de>
2015-09-25 14:18:12 +0200

    extract time series from a bunch of co-occurence things

--- java/main/org/hedera/mapreduce/BuildShortTermTS.java
+++ java/main/org/hedera/mapreduce/BuildShortTermTS.java
@@ -1,7 +1,8 @@
 package org.hedera.mapreduce;
 
-import gnu.trove.map.hash.TObjectIntHashMap;
-import gnu.trove.procedure.TObjectIntProcedure;
+import java.io.IOException;
+import java.util.Arrays;
+
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.cli.Options;
@@ -9,8 +10,11 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.WritableComparator;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Partitioner;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
@@ -22,161 +26,308 @@ import org.apache.hadoop.util.ToolRunner;
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
+
+import gnu.trove.map.hash.TObjectIntHashMap;
+import gnu.trove.procedure.TObjectIntProcedure;
 import tl.lin.data.pair.PairOfStrings;
 import tuan.hadoop.conf.JobConfig;
 
-import java.io.IOException;
-import java.util.Arrays;
-
 /**
- * Created by tuan on 16/09/15.
+ * Build the short-term time series of mutual information between entities
+ * Input: src TAB dest TAB timestamp TAB anchor TAB title TAB offset TAB pre TAB post
+ * Output: e1 TAB e2 TAB date TAB count 
+ * 
+ * @author tuan
  */
 public class BuildShortTermTS extends JobConfig implements Tool {
-    private static final DateTimeFormatter dtf = DateTimeFormat.forPattern("YYYY-MM-dd");
 
-    private static final String PHASE = "phase";
-    private static final String PHASE_OPT = "phase.opt";
+	private static final DateTimeFormatter dtf = DateTimeFormat.forPattern("YYYY-MM-dd");
 
-    // Step 1: flatten all destinations into one line
-    private static class MyMapper extends Mapper<LongWritable, Text, PairOfStrings, Text> {
+	private static final String PHASE = "phase";
+	private static final String PHASE_OPT = "phase.opt";
+	
+	// This option is only applicable for phase 3
+	private static final String BEGIN_TIME = "begin";
+	private static final String END_TIME = "end";
 
-        private final PairOfStrings KEY = new PairOfStrings();
-        private final Text VAL = new Text();
+	// Step 1: flatten all destinations into one line
+	private static class MyMapper extends Mapper<LongWritable, Text, PairOfStrings, Text> {
 
-        @Override
-        protected void map(LongWritable key, Text value, Context context)
-                throws IOException, InterruptedException {
+		private final PairOfStrings KEY = new PairOfStrings();
+		private final Text VAL = new Text();		
 
-            String line = value.toString();
-            int i = line.indexOf('\t');
-            int j = line.indexOf('\t',i+1);
-            int k = line.indexOf('\t',j+1);
+		@Override
+		protected void map(LongWritable key, Text value, Context context)
+				throws IOException, InterruptedException {
 
-            long timestamp = Long.parseLong(line.substring(j+1, k));
-            DateTime dt = new DateTime(timestamp);
+			String line = value.toString();
+			int i = line.indexOf('\t');
+			int j = line.indexOf('\t',i+1);
+			int k = line.indexOf('\t',j+1);			
 
-            KEY.set(line.substring(0, i), dt.toString(dtf));
-            VAL.set(line.substring(i+1, j));
+			long timestamp = Long.parseLong(line.substring(j+1, k));
+			DateTime dt = new DateTime(timestamp);
 
-            context.write(KEY, VAL);
-        }
-    }
+			KEY.set(line.substring(0, i), dt.toString(dtf));
+			VAL.set(line.substring(i+1, j));
 
-    private static class MyReducer extends Reducer<PairOfStrings, Text, PairOfStrings, IntWritable> {
+			context.write(KEY, VAL);
+		}	
+	}
 
-        private final PairOfStrings KEY = new PairOfStrings();
-        private final IntWritable VAL = new IntWritable();
+	private static class MyReducer extends Reducer<PairOfStrings, Text, PairOfStrings, IntWritable> {
 
-        @Override
-        protected void reduce(final PairOfStrings key, Iterable<Text> vals, final Context context)
-                throws IOException, InterruptedException {
+		private final PairOfStrings KEY = new PairOfStrings();
+		private final IntWritable VAL = new IntWritable();
 
-            TObjectIntHashMap<String> idx = new TObjectIntHashMap<>();
-            for (Text val : vals) {
-                idx.adjustOrPutValue(val.toString(), 1, 1);
-            }
+		@Override
+		protected void reduce(final PairOfStrings key, Iterable<Text> vals, final Context context) 
+				throws IOException, InterruptedException {
 
-            // Now if we are not crashed yet, move on
-            TObjectIntHashMap<String> mi = combinations(idx);
-            if (mi == null) return;
+			TObjectIntHashMap<String> idx = new TObjectIntHashMap<>();
+			for (Text val : vals) {
+				idx.adjustOrPutValue(val.toString(), 1, 1);
+			}
 
-            mi.forEachEntry(new TObjectIntProcedure<String>() {
-                public boolean execute(String k, int v) {
-                    try {
-                        KEY.set(k, key.getRightElement());
-                        VAL.set(v);
-                        context.write(KEY, VAL);
-                    } catch (IOException | InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    return true;
-                }
-            });
-        }
-    }
+			// Now if we are not crashed yet, move on
+			TObjectIntHashMap<String> mi = combinations(idx);
 
-    @SuppressWarnings("static-access")
-    @Override
-    public Options options() {
-        Options opts = super.options();
-        Option pOpt = OptionBuilder.withArgName("phase").hasArg(true).isRequired(false)
-                .withDescription("phase [aggr|cnt]").create(PHASE);
-        opts.addOption(pOpt);
-        return opts;
-    }
-    @Override
-    public int run(String[] args) throws Exception {
-        parseOtions(args);
-        String phase = "aggr";
+			mi.forEachEntry(new TObjectIntProcedure<String>() {
+				public boolean execute(String k, int v) {					
+					try {
+						KEY.set(k, key.getRightElement());
+						VAL.set(v);
+						context.write(KEY, VAL);						
+					} catch (IOException | InterruptedException e) {					
+						e.printStackTrace();
+					}
+					return true;
+				}				
+			});
+		}
+	}
 
-        if (command.hasOption(PHASE)) {
-            phase = command.getOptionValue(PHASE);
-        }
+	private static class MyPartitioner extends Partitioner<PairOfStrings, IntWritable> {
 
-        if ("aggr".equals(phase)) {
-            phase1(args);
-        }
-        else if ("cnt".equals(phase)) {
-            phase2(args);
-        }
-        return 0;
-    }
+		@Override
+		public int getPartition(PairOfStrings key, IntWritable val, int numReduceTasks) {
+			return (key.getRightElement().hashCode() % Integer.MAX_VALUE) % numReduceTasks;
+		}
+	}
 
-    private int phase1(String[] args) throws InterruptedException,
-            IOException, ClassNotFoundException {
-        System.out.println("Phase 1");
-        Job job = setup(TextInputFormat.class, SequenceFileOutputFormat.class,
-                PairOfStrings.class, Text.class,
-                PairOfStrings.class, IntWritable.class,
-                MyMapper.class, MyReducer.class, args);
-        Configuration conf = job.getConfiguration();
-        conf.set("mapreduce.map.memory.mb", "3000");
-        conf.set("mapreduce.map.java.opts", "-Xmx3000m");
-        conf.set("mapreduce.reduce.memory.mb", "3000");
-        conf.set("mapreduce.reduce.java.opts", "-Xmx3000m");
-        job.waitForCompletion(true);
-        return 0;
-    }
+	private static class MyGroupingComparator extends WritableComparator {
 
-    private int phase2(String[] args) throws InterruptedException,
-            IOException, ClassNotFoundException {
-        System.out.println("Phase 1");
-        Job job = setup(SequenceFileInputFormat.class, TextOutputFormat.class,
-                PairOfStrings.class, IntWritable.class,
-                PairOfStrings.class, IntWritable.class,
-                Mapper.class, IntSumReducer.class, args);
-        Configuration conf = job.getConfiguration();
-        conf.set("mapreduce.map.memory.mb", "3000");
-        conf.set("mapreduce.map.java.opts", "-Xmx3000m");
-        conf.set("mapreduce.reduce.memory.mb", "3000");
-        conf.set("mapreduce.reduce.java.opts", "-Xmx3000m");
-        job.waitForCompletion(true);
-        return 0;
-    }
+		protected MyGroupingComparator() {
+			super(PairOfStrings.class, true);
+		}
+		
+		@Override
+		public int compare(WritableComparable a, WritableComparable b) {
+			PairOfStrings k1 = (PairOfStrings)a;
+			PairOfStrings k2 = (PairOfStrings)b;
+			return k1.getLeftElement().compareTo(k2.getLeftElement());
+		}
+	}
 
-    private static TObjectIntHashMap<String> combinations(TObjectIntHashMap<String> maps) {
-        String[] keys = maps.keys(new String[maps.size()]);
-        Arrays.sort(keys);
-        TObjectIntHashMap<String> results = new TObjectIntHashMap<>(maps.size() * (maps.size()-1)/2);
+	private static class MySortingComparator extends WritableComparator {
 
-        for (int i=0; i<keys.length-1;i++) {
-            for (int j=i+1; j<keys.length;j++) {
-                if (keys[i].equals(keys[j])) continue;
-                String k = keys[i] + "@@" + keys[j];
-                results.adjustOrPutValue(k, Math.min(maps.get(keys[i]), maps.get(keys[j])),
-                        Math.min(maps.get(keys[i]), maps.get(keys[j])));
-            }
-        }
-        return (results.size() == 0) ? null : results;
-    }
+		protected MySortingComparator() {
+			super(PairOfStrings.class, true);
+		}
+		
+		@Override
+		public int compare(WritableComparable a, WritableComparable b) {	
+			PairOfStrings k1 = (PairOfStrings)a;
+			PairOfStrings k2 = (PairOfStrings)b;
 
-    public static void main(String[] args) {
-        try {
-            ToolRunner.run(new BuildShortTermTS(), args);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-		/*DateTime dt = new DateTime(1418431632000l);
-		System.out.println(dt.toString(dtf));*/
-    }
+			int compare = k1.getLeftElement().compareTo(k2.getLeftElement());			
+			if (compare == 0) {
+				return k1.getRightElement().compareTo(k2.getRightElement());
+			}
+			return compare;
+		}
+	}
+	
+	private static class MyReducer1 extends Reducer<PairOfStrings, IntWritable, Text, Text> {		
+
+		private final Text KEY = new Text();
+		private final Text VALUE = new Text();
+		
+		// This is nasty: We have to use global variables to do our job
+		private DateTime begin = null, end = null, curDate = null;
+		private String prevPair = null;
+		private StringBuilder ts = new StringBuilder();
+		
+		@Override
+		protected void setup(Context context) throws IOException, InterruptedException {
+			super.setup(context);			
+			Configuration conf = context.getConfiguration();
+			begin = dtf.parseDateTime("2015-01-30");
+			end = dtf.parseDateTime("2015-03-02");
+		}
+
+		@Override
+		protected void reduce(PairOfStrings key, Iterable<IntWritable> vals, Context context)
+						throws IOException, InterruptedException {
+			
+			// Sum before doing any fancy stuff
+			int sum = 0;
+			for (IntWritable v : vals) sum += v.get();
+			
+			if (prevPair == null || !prevPair.equals(key.getLeftElement())) {				
+				curDate = begin;				
+				if (prevPair != null) {
+					KEY.set(prevPair);
+					VALUE.set(ts.toString());
+					ts.delete(0, ts.length());
+					context.write(KEY, VALUE);
+				}
+				prevPair = key.getLeftElement();
+				
+				DateTime dt = dtf.parseDateTime(key.getRightElement());
+				while (curDate.isBefore(dt)) {
+					ts.append(String.valueOf(0));
+					ts.append(' ');
+					curDate = curDate.plusDays(1);
+				}
+				ts.append(String.valueOf(sum));
+				ts.append(' ');
+			}
+			else {
+				DateTime dt = dtf.parseDateTime(key.getRightElement());
+				while (curDate.isBefore(dt)) {
+					ts.append(String.valueOf(0));
+					ts.append(' ');
+					curDate = curDate.plusDays(1);
+				}
+				ts.append(String.valueOf(sum));
+				ts.append(' ');
+			}
+		}
+		
+		@Override
+		// Emit one last point
+		protected void cleanup(Context context) throws IOException, InterruptedException {
+			if (prevPair != null) {
+				KEY.set(prevPair);
+				VALUE.set(ts.toString());
+				context.write(KEY, VALUE);
+			}
+			ts.delete(0, ts.length());
+			prevPair = null;
+			begin = end = curDate = null;
+		}
+	}
+
+	@SuppressWarnings("static-access")
+	@Override
+	public Options options() {
+		Options opts = super.options();
+	
+		Option pOpt = OptionBuilder.withArgName("phase").hasArg(true).isRequired(false)
+				.withDescription("phase [aggr|cnt|ts]").create(PHASE);
+		opts.addOption(pOpt);
+		
+		Option bOpt = OptionBuilder.withArgName("begin").hasArg(true).isRequired(false)
+				.withDescription("beginning time (format YYYY-MM-dd)").create(BEGIN_TIME);
+		opts.addOption(bOpt);
+		
+		Option eOpt = OptionBuilder.withArgName("end").hasArg(true).isRequired(false)
+				.withArgName("ending time (format YYYY-MM-dd)").create(END_TIME);
+		opts.addOption(eOpt);
+		
+		return opts;
+	}
+	@Override
+	public int run(String[] args) throws Exception {
+		parseOtions(args);
+		String phase = "aggr";
+
+		if (command.hasOption(PHASE)) {
+			phase = command.getOptionValue(PHASE);
+		}
+
+		if ("aggr".equals(phase)) {
+			phase1(args);
+		}
+		else if ("cnt".equals(phase)) {
+			phase2(args);
+		}
+		else if ("ts".equals(phase)) {
+			phase3(args);
+		}
+		return 0;
+	}
+
+	private int phase1(String[] args) throws InterruptedException,
+	IOException, ClassNotFoundException {
+		System.out.println("Phase 1");
+		Job job = setup(TextInputFormat.class, SequenceFileOutputFormat.class,
+				PairOfStrings.class, Text.class, PairOfStrings.class, IntWritable.class,
+				MyMapper.class, MyReducer.class, args);
+		job.waitForCompletion(true);
+		return 0;
+	}
+
+	private int phase2(String[] args) throws InterruptedException,
+	IOException, ClassNotFoundException {
+		System.out.println("Phase 2");
+		Job job = setup(SequenceFileInputFormat.class, SequenceFileOutputFormat.class,
+				PairOfStrings.class, IntWritable.class, PairOfStrings.class, IntWritable.class,
+				Mapper.class, IntSumReducer.class, IntSumReducer.class, args);
+		job.waitForCompletion(true);
+		return 0;
+	}
+
+	private int phase3(String[] args) throws InterruptedException,
+	IOException, ClassNotFoundException {
+		System.out.println("Phase 3");
+		Job job = setup(SequenceFileInputFormat.class, TextOutputFormat.class,
+				PairOfStrings.class, IntWritable.class, PairOfStrings.class, IntWritable.class,
+				Mapper.class, MyReducer1.class, args);
+		Configuration conf = job.getConfiguration();
+		
+		String beginTime = "2015-01-30";
+		if (command.hasOption(BEGIN_TIME)) {
+			beginTime = command.getOptionValue(BEGIN_TIME);
+			conf.set(BEGIN_TIME, beginTime);
+		}
+		
+		String endTime = "2015-03-02";
+		if (command.hasOption(END_TIME)) {
+			endTime = command.getOptionValue(END_TIME);
+			conf.set(END_TIME, endTime);
+		}
+		
+		job.setPartitionerClass(MyPartitioner.class);
+		job.setGroupingComparatorClass(MyGroupingComparator.class);
+		job.setSortComparatorClass(MySortingComparator.class);
+		
+		job.waitForCompletion(true);
+		return 0;
+	}
+
+	private static TObjectIntHashMap<String> combinations(TObjectIntHashMap<String> maps) {
+		String[] keys = maps.keys(new String[maps.size()]);
+		Arrays.sort(keys);		
+		TObjectIntHashMap<String> results = new TObjectIntHashMap<>(maps.size() * (maps.size()-1)/2);
+
+		for (int i=0; i<keys.length-1;i++) {			
+			for (int j=i+1; j<keys.length;j++) {
+				if (keys[i].equals(keys[j])) continue;
+				String k = keys[i] + "@@" + keys[j];
+				results.adjustOrPutValue(k, Math.min(maps.get(keys[i]), maps.get(keys[j])), 
+						Math.min(maps.get(keys[i]), maps.get(keys[j])));
+			}
+		}
+		return (results.size() == 0) ? null : results;
+	}
+
+	public static void main(String[] args) {
+		try {
+			ToolRunner.run(new BuildShortTermTS(), args);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
